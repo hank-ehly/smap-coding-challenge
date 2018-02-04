e12e756bd075480013dafb75425a3e650d8fb939
Hank Ehly <henry.ehly@gmail.com>
2018-02-04 17:58:08 +0900

    Add user data import functionality

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,8 +1,21 @@
 from django.core.management.base import BaseCommand
 
+import pandas as pd
+
+from dashboard.settings import USER_DATA_PATH
+from consumption.models import User
+
+
+def import_users(user_data_path=USER_DATA_PATH):
+    df = pd.read_csv(user_data_path)
+
+    User.objects.bulk_create(
+        User(**vals) for vals in df.to_dict('records')
+    )
+
 
 class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        print("Implement me!")
+        import_users()
