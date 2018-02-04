973cb031249a3b914b0cfc78e9f96f92df2aa484
Hank Ehly <henry.ehly@gmail.com>
2018-02-04 17:34:08 +0900

    Create models

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class User(models.Model):
+    area = models.CharField(max_length=2)
+    tariff = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.pk
+
+
+@python_2_unicode_compatible
+class Consumption(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField('Time of consumption')
+    consumption = models.FloatField()
+
+    def __str__(self):
+        return "%s - %s" % (self.datetime, self.consumption)
